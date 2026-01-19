1373c5c22bd840a2d7878dd0d9c37d1f27371378
superstephgreat <superstephdev@gmail.com>
2026-01-19 10:03:34 +0100

    Added theme toggle functionaliy

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 console.log('Calculator script loaded')
 
 const currentOperand = document.querySelector('.current-operand');
@@ -10,6 +11,60 @@ const operatorBtns = document.querySelectorAll('.operator');
 const numberBtns = document.querySelectorAll('.number');
 const equalsBtn = document.querySelector('.equals');
 
+
+// Theme Toggle 
+const sunSVG = `
+<svg class="theme-icon sun" width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="12" cy="12" r="4" stroke="currentColor" stroke-width="1.8" />
+  <line x1="12" y1="1" x2="12" y2="4" stroke="currentColor" stroke-width="1.8" />
+  <line x1="12" y1="20" x2="12" y2="23" stroke="currentColor" stroke-width="1.8" />
+  <line x1="1" y1="12" x2="4" y2="12" stroke="currentColor" stroke-width="1.8" />
+  <line x1="20" y1="12" x2="23" y2="12" stroke="currentColor" stroke-width="1.8" />
+  <line x1="4.5" y1="4.5" x2="6.8" y2="6.8" stroke="currentColor" stroke-width="1.8" />
+  <line x1="17.2" y1="17.2" x2="19.5" y2="19.5" stroke="currentColor" stroke-width="1.8" />
+  <line x1="4.5" y1="19.5" x2="6.8" y2="17.2" stroke="currentColor" stroke-width="1.8" />
+  <line x1="17.2" y1="6.8" x2="19.5" y2="4.5" stroke="currentColor" stroke-width="1.8" />
+</svg>
+`;
+
+const moonSVG = `
+<svg class="theme-icon moon" width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M21 12.5C20 17 15.5 20.5 11 20C6.5 19.5 3 15.5 3.5 11 C4 6.5 7.5 3 12 3.5 C9.5 5 8 8.5 9 12 C10 15.5 13.5 18 17 17 C18.5 16.7 20 15.8 21 14.5Z"
+    stroke="currentColor"
+    stroke-width="1.8"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
+`;
+
+const html = document.documentElement;
+const toggleBtn = document.querySelector('.theme-switch');
+
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+if (prefersDarkScheme.matches) {
+  html.setAttribute('data-theme', 'dark');
+  toggleBtn.innerHTML = moonSVG;
+} else {
+  html.setAttribute('data-theme', 'light');
+  toggleBtn.innerHTML = sunSVG;
+}
+
+toggleBtn.addEventListener("click", () => {
+  const currentTheme = html.getAttribute("data-theme");
+
+  if (currentTheme === "dark") {
+    html.setAttribute("data-theme", "light");
+    toggleBtn.innerHTML = sunSVG;
+  } else {
+    html.setAttribute("data-theme", "dark");
+    toggleBtn.innerHTML = moonSVG;
+  }
+});
+
+
+// Calculator Logic
 let expression =[];
 let currentValue = '';
 
